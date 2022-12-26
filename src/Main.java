1efdea4545abd0b699cd57ace5c3085f827c21c0
Anastasia test <gorandi@inbox.ru>
2022-12-26 18:48:44 +0300

    add Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 80;
+        int mile = 20;
+        int milesAccrual = price / mile;
+        System.out.println(milesAccrual);
+    }
+}
